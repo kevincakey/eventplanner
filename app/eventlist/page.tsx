b32d707e0e79eadf85c46ab4f8f9521296e55582
Kevin Gao <okevin.gao@gmail.com>
2024-06-17 22:32:09 -0400

    currently working on separating the form and the events list

--- app/eventlist/page.tsx
+++ app/eventlist/page.tsx
@@ -1,35 +1,12 @@
-// app/page.tsx
-import { createClient } from "@/utils/supabase/server";
+import EventsList from '../../components/EventsList'
+import EventForm from '../../components/EventForm'
 
 export default async function Page() {
-  // Fetch data from Supabase
-  const supabase = createClient();
-  const { data: eventslist, error } = await supabase
-    .from("eventslist")
-    .select();
 
-  // Check for errors
-  if (error) {
-    console.error("Error fetching events list:", error);
-    return <div>Error loading events.</div>;
-  }
-
-  // If no data, return a message
-  if (!eventslist || eventslist.length === 0) {
-    return <div>No events found.</div>;
-  }
-
-  // Render the list of events
   return (
     <div>
-      <h1>Events List</h1>
-      <ul>
-        {eventslist.map((event, index) => (
-          <li key={index}>
-            <pre>{JSON.stringify(event, null, 2)}</pre>
-          </li>
-        ))}
-      </ul>
+      <EventForm/>
+      <EventsList/>
     </div>
   );
 }
